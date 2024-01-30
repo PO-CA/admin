5d473005e9c7697c022953bccf40d97d23830b63
kkh <kkhdevs@gmail.com>
2024-01-30 14:24:54 +0900

    ad

--- constants/apis.ts
+++ constants/apis.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'http://54.180.31.61:8080';
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'http://54.180.31.61:8080';
+// export const API_URL = 'http://localhost:8080';
