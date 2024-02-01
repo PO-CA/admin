637e172fbe577117fd7477d4d39baf7c958452f5
kkh <kkhdevs@gmail.com>
2024-02-01 15:01:05 +0900

    add meta

--- constants/apis.ts
+++ constants/apis.ts
@@ -1,3 +1,3 @@
 // export const API_URL =
-// 'https://port-0-api-server-199u12dlrzxaod6.sel5.cloudtype.app';
-export const API_URL = 'http://localhost:8080';
+'https://port-0-api-server-199u12dlrzxaod6.sel5.cloudtype.app';
+// export const API_URL = 'http://localhost:8080';
