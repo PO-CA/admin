7df3783ae2aa06d8564fdbb69db496c77120162a
kkh <kkhdevs@gmail.com>
2024-04-15 15:51:24 +0900

    add channel

--- components/table/index.tsx
+++ components/table/index.tsx
@@ -71,7 +71,7 @@ export default function TanTable({
                 table.setPageSize(Number(e.target.value));
               }}
             >
-              {[10, 20, 30, 40, 50].map((pageSize) => (
+              {[20, 30, 40, 50].map((pageSize) => (
                 <option key={pageSize} value={pageSize}>
                   {pageSize}
                 </option>
