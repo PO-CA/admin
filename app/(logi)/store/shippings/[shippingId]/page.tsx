bb67040fa2328e372b9693d7ec0a265171955f5a
kkh <kkhdevs@gmail.com>
2024-06-20 00:34:56 +0900

    fix staff invoice in store

--- app/(logi)/store/shippings/[shippingId]/page.tsx
+++ app/(logi)/store/shippings/[shippingId]/page.tsx
@@ -1,6 +1,23 @@
 'use client';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './page.module.css';
 import { useGetAllShippingsByShippingId } from '@/query/query/shippings';
+import {
+  ColumnFiltersState,
+  getCoreRowModel,
+  getFacetedMinMaxValues,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { shippingItemColumns } from '@/app/(admin)/shippings/(components)/tableColumns/shippingItemColumns';
+import TanTable, { fuzzyFilter } from '@/components/table';
+import { useReactToPrint } from 'react-to-print';
+import { useRouter } from 'next/navigation';
+import tableStyles from './table.module.css';
 
 export default function ShippingDetail({
   params,
@@ -14,6 +31,194 @@ export default function ShippingDetail({
     isLoading: isShippingLoading,
     isSuccess: isShippingSuccess,
   } = useGetAllShippingsByShippingId(shippingId);
+  const router = useRouter();
+
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [globalFilter, setGlobalFilter] = useState('');
+
+  const [invoiceData, setInvoiceData] = useState<any>([]);
+
+  useEffect(() => {
+    if (shippingData) {
+      setInvoiceData([
+        ...shippingData?.logiShippingItems,
+        {},
+        {},
+        {
+          productName: '배송비',
+          totalPrice: shippingData?.shippingFee,
+        },
+        {
+          productName: 'Total Amount in',
+          totalPrice:
+            shippingData?.totalProductPrice + shippingData?.shippingFee,
+        },
+      ]);
+    }
+  }, [shippingData]);
+
+  const table = useReactTable({
+    data: invoiceData,
+    columns: shippingItemColumns,
+    filterFns: {
+      fuzzy: fuzzyFilter,
+    },
+    initialState: {
+      pagination: { pageSize: 500, pageIndex: 0 },
+    },
+    state: {
+      columnFilters,
+      globalFilter,
+    },
+    onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: fuzzyFilter,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    getFacetedMinMaxValues: getFacetedMinMaxValues(),
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: false,
+  });
+
+  const componentRef = useRef<ComponentToPrint>(null);
+
+  const handlePrint = useReactToPrint({
+    content: (): any => componentRef.current,
+    onAfterPrint: () => router.back(),
+  });
+
+  const printGoback = () => {
+    handlePrint();
+  };
+  class ComponentToPrint extends React.Component {
+    render() {
+      return (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            backgroundColor: 'white',
+            margin: '10px',
+          }}
+        >
+          <div
+            style={{
+              fontSize: '30px',
+              fontWeight: 900,
+              margin: '20px 0 0 30px',
+            }}
+          >
+            INVOICE
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              marginLeft: '40px',
+              marginTop: '20px',
+              marginBottom: '100px',
+              fontSize: '18px',
+            }}
+          >
+            <div style={{ flex: '0.5' }}>
+              <div style={{ fontWeight: 900, marginBottom: '5px' }}>Seller</div>
+              <div>HMcompany</div>
+              <div>71, Bukhang-ro 207beon-gil</div>
+              <div>Seo-gu, Incheon, Korea, 22856 </div>
+              <div style={{ marginTop: '10px' }}>Tel: +82 2 010 5788 7679</div>
+            </div>
+            <div style={{ flex: '0.5' }}>
+              <div>invoice No.: {shippingData?.id}</div>
+              <div>
+                invoice Date: {new Date().toISOString().substring(0, 10)}
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <TanTable
+              table={table}
+              globalFilter={globalFilter}
+              setGlobalFilter={setGlobalFilter}
+              styles={tableStyles}
+            />
+            <div style={{ padding: '0px 21.5px', display: 'flex' }}>
+              <div
+                style={{
+                  flex: '0.65',
+                  textAlign: 'center',
+                  fontSize: '18px',
+                  fontWeight: 900,
+                  padding: '5px 0px',
+                  border: '1px solid lightgray',
+                }}
+              >
+                {'Declaration of origin'}
+              </div>
+              <div
+                style={{
+                  flex: '0.35',
+                  textAlign: 'center',
+                  fontSize: '18px',
+                  fontWeight: 900,
+                  padding: '5px 0px',
+                  border: '1px solid lightgray',
+                }}
+              >
+                {'Date & Company Chop'}
+              </div>
+            </div>
+            <div style={{ padding: '0px 21.5px', display: 'flex' }}>
+              <div
+                style={{
+                  flex: '0.65',
+                  textAlign: 'center',
+                  fontSize: '16px',
+                  fontWeight: 900,
+                  padding: '5px 0px',
+                  border: '1px solid lightgray',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <div>
+                  {'We the undersigned, the exporter of the products, '}
+                </div>
+                <div>{'covered by this document, declare that, '}</div>
+                <div>{'except where otherwise clearly indicated,'}</div>
+                <div>
+                  {'these products are of South Korea preferntial origin.'}
+                </div>
+              </div>
+              <div
+                style={{
+                  flex: '0.35',
+                  textAlign: 'center',
+                  fontSize: '16px',
+                  fontWeight: 900,
+                  padding: '5px 0px 0px',
+                  border: '1px solid lightgray',
+                }}
+              >
+                <div style={{ height: '100px' }}></div>
+                <div
+                  style={{ borderTop: '1px solid lightgray', padding: '10px' }}
+                >
+                  HMcompany
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
 
   if (isShippingLoading) return <div>loading</div>;
 
@@ -21,66 +226,8 @@ export default function ShippingDetail({
 
   return (
     <main className={styles.productDetailContainer}>
-      <div>배송-상세</div>
-      <div>
-        <div>
-          <div>배송번호</div>
-          <div>{shippingData?.id}</div>
-        </div>
-        <div>
-          <div>주소</div>
-          <div>{shippingData?.city}</div>
-        </div>
-        <div>
-          <div>상세주소</div>
-          <div>{shippingData?.street}</div>
-        </div>
-        <div>
-          <div>수령인</div>
-          <div>{shippingData?.receiverName}</div>
-        </div>
-        <div>
-          <div>연락처</div>
-          <div>{shippingData?.receiverPhoneNumber}</div>
-        </div>
-        <div>
-          <div>우편번호</div>
-          <div>{shippingData?.zipcode}</div>
-        </div>
-        <div>
-          <div>배송상태</div>
-          <div>{shippingData?.shippingStatus}</div>
-        </div>
-        <div>
-          <div>배송방법</div>
-          <div>{shippingData?.shippingType}</div>
-        </div>
-        <div>
-          <div>상품수량</div>
-          <div>{shippingData?.totalQty}</div>
-        </div>
-        <div>
-          <div>상품가격</div>
-          <div>{shippingData?.totalProductPrice}</div>
-        </div>
-        <div>
-          <div>배송비</div>
-          <div>{shippingData?.shippingFee}</div>
-        </div>
-        <div>
-          <div>총비용</div>
-          <div>
-            {shippingData?.shippingFee + shippingData?.totalProductPrice}
-          </div>
-        </div>
-      </div>
-      {/* <TanTable
-        data={shippingData?.logiShippingItems}
-        columns={shippingItemColumns}
-        useSearch={false}
-        useFilter={false}
-        usePagenation={false}
-      /> */}
+      <button onClick={printGoback}>인쇄하기</button>
+      <ComponentToPrint ref={componentRef} />
     </main>
   );
 }
